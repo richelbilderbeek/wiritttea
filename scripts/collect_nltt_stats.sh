ca5964c8f6e9bc08dec0415c587efc08bb9f948e
richelbilderbeek <richel@richelbilderbeek.nl>
2016-11-01 11:22:20 +0100

    Increase time

--- scripts/collect_nltt_stats.sh
+++ scripts/collect_nltt_stats.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=10:00:00
+#SBATCH --time=100:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
-#SBATCH --mem=10G
+#SBATCH --mem=100G
 #SBATCH --job-name=collect_nltt_stats
 #SBATCH --output=collect_nltt_stats.log
 module load R/3.3.1-foss-2016a
