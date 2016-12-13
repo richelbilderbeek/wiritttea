a3278f27d5791a2e0b53cc86ec663c85d9d7f961
richelbilderbeek <richel@richelbilderbeek.nl>
2016-12-13 15:47:43 +0100

    Deeper analysis

--- R/read_collected_n_taxa.R
+++ R/read_collected_n_taxa.R
@@ -1,9 +1,11 @@
 #' Reads the file with the collected number of taxa
+#' @param csv_filename name of the CSV file
 #' @return a data frame with all parameters
 #' @author Richel Bilderbeek
 #' @export
-read_collected_n_taxa <- function() {
-  csv_filename <- wiritttea::find_path("collect_files_n_taxa.csv")
+read_collected_n_taxa <- function(
+  csv_filename = wiritttea::find_path("collect_files_n_taxa.csv")
+) {
   testit::assert(file.exists(csv_filename))
   df <- utils::read.csv(
     file = csv_filename,
