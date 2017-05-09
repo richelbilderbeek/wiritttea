69e61f39ff461b0ffe8c2f6affae0b2fd1225e60
richelbilderbeek <richel@richelbilderbeek.nl>
2017-05-09 09:59:21 +0200

    EOD

--- demo/analyse_n_taxa.R
+++ demo/analyse_n_taxa.R
@@ -1,93 +1,57 @@
----
-title: "Analyse the number of taxa"
-author: "Richel Bilderbeek"
-date: "`r Sys.Date()`"
-output: rmarkdown::html_vignette
-vignette: >
-  %\VignetteIndexEntry{Analyse the number of taxa}
-  %\VignetteEngine{knitr::rmarkdown}
-  %\VignetteEncoding{UTF-8}
----
+# Analyse the number of taxa
+options(warn = 2) # Be strict
+path_data <- "~/Peregrine20170208"
+n_taxa_filename <- "~/n_taxa.csv"
 
-Loading this package
-
-```{r}
-library(wiritttea)
-options(warn = 2)
-```
-
-
-Loading the number of taxa:
-
-```{r}
-my_filenames <- list.files("~/Peregrine20170208", pattern="*.RDa", full.names = TRUE)
-sub_filenames <- my_filenames
-df_n_taxa <- wiritttea::collect_files_n_taxa(filenames = sub_filenames)
-write.csv(df_n_taxa, "~/n_taxa.csv")
-```
-
-Profiling:
-
-```{r}
-if (1==2){
-rprof_tmp_output <- "~/tmp_rprof"
-Rprof(rprof_tmp_output)
-wiritttea::collect_files_n_taxa(filenames = sub_filenames)
-Rprof(NULL)
-summaryRprof(rprof_tmp_output)
+# Create file if absent
+if (!file.exists(n_taxa_filename)) {
+  my_filenames <- list.files(path_data, pattern="*.RDa", full.names=TRUE)
+  write.csv(wiritttea::collect_files_n_taxa(filenames = my_filenames), n_taxa_filename)
 }
-```
 
-Loading the parameters:
+# Investigations
+df_n_taxa <- wiritttea::read_collected_n_taxa(n_taxa_filename)
 
-```{r}
-df_parameters <- wiritttea::read_collected_parameters()
-```
-
-## The success rate
-
-```{r fig.width = 7, fig.height = 7}
-n_fail <- sum( is.na(df_n_taxa$n_taxa))
+# The success rate
+n_fail <- sum(is.na(df_n_taxa$n_taxa))
 n_success <- sum(!is.na(df_n_taxa$n_taxa))
 df_success <- data.frame(
   name = c("success", "fail"), n = c(n_success, n_fail)
 )
-
 ggplot2::ggplot(
-  df_success, 
+  df_success,
   ggplot2::aes(
-    x = factor(""), 
-    y = df_success$n, 
+    x = factor(""),
+    y = df_success$n,
     fill = df_success$name
   )
-) + 
-  ggplot2::geom_bar(stat = "identity") + 
+) +
+  ggplot2::geom_bar(stat = "identity") +
   ggplot2::scale_x_discrete("") +
   ggplot2::scale_y_continuous("number of parameter sets") +
-  ggplot2::coord_polar(theta = "y") + 
+  ggplot2::coord_polar(theta = "y") +
   ggplot2::ggtitle("Incipient species trees created")
-```
 
-## What is the global distribution of number of taxa?
+# What is the global distribution of number of taxa?
 
-Plot the distribution of number of taxa (in the reconstructed incipient species tree) over all runs:
+if (1 == 2) {
+  #Plot the distribution of number of taxa (in the reconstructed incipient species tree) over all runs
+  df <- df_n_taxa[ !is.na(df_n_taxa$n_taxa), ]
+  df_na <- df_n_taxa[ is.na(df_n_taxa$n_taxa), ]
 
-```{r fig.width = 7, fig.height = 7}
-df <- df_n_taxa[ !is.na(df_n_taxa$n_taxa), ]
+  n_missing <- sum(is.na(df_n_taxa$n_taxa))
 
-n_missing <- sum(is.na(df_n_taxa$n_taxa))
-ggplot2::ggplot(
-  data = df, ggplot2::aes(df$n_taxa)
-) + 
-ggplot2::geom_histogram(bins = 30) + 
-ggplot2::ggtitle(
-  paste0(
-    "Number of taxa for ", n_success, "/",
-    nrow(df_n_taxa) ," runs"
+  ggplot2::ggplot(
+    data = df, ggplot2::aes(df$n_taxa)
+  ) +
+  ggplot2::geom_histogram(bins = 30) +
+  ggplot2::ggtitle(
+    paste0(
+      "Number of taxa for ", n_success, "/",
+      nrow(df_n_taxa) ," runs"
+    )
   )
-)
-```
-
+}
 
 ## Which parameters?
 
@@ -96,7 +60,7 @@ taxa:
 
 ```{r}
 # Create a 'filename' column needed for merge
-df_parameters$filename <- rownames(df_parameters) 
+df_parameters$filename <- rownames(df_parameters)
 # Outer join on the filename
 df <- merge(df_parameters, df_n_taxa, by = "filename")
 # Either remove the NA's...
@@ -110,14 +74,28 @@ Here we plot both successes and fails. The latter are only plot if present.
 ```{r fig.width = 7, fig.height = 7}
 if (1==2) {
 if (nrow(df_fail) == 0) {
-  ggplot2::ggplot(data = df, ggplot2::aes(x = df$sirg, y = df$n_taxa, color = as.factor(df$erg))) + 
+  ggplot2::ggplot(data = df, ggplot2::aes(x = df$sirg, y = df$n_taxa, color = as.factor(df$erg))) +
   ggplot2::geom_point(shape = 16) +
-  ggplot2::ggtitle("Effect of speciation and extinction rates on #taxa") 
+  ggplot2::ggtitle("Effect of speciation and extinction rates on #taxa")
 } else {
-  ggplot2::ggplot(data = df, ggplot2::aes(x = df$sirg, y = df$n_taxa, color = as.factor(df$erg))) + 
+  ggplot2::ggplot(data = df, ggplot2::aes(x = df$sirg, y = df$n_taxa, color = as.factor(df$erg))) +
   ggplot2::geom_point(shape = 16) +
-  ggplot2::geom_jitter(data = df_fail, shape = 3, width = 0.02, height = 0.02, ggplot2::aes(x = df_fail$sirg, y = 0,   color = as.factor(df_fail$erg))) + 
-  ggplot2::ggtitle("Effect of speciation and extinction rates on #taxa (crosses denote NA)") 
+  ggplot2::geom_jitter(data = df_fail, shape = 3, width = 0.02, height = 0.02, ggplot2::aes(x = df_fail$sirg, y = 0,   color = as.factor(df_fail$erg))) +
+  ggplot2::ggtitle("Effect of speciation and extinction rates on #taxa (crosses denote NA)")
 }
 }
 ```
+
+
+
+
+# Profiling
+
+if (1==2){
+  rprof_tmp_output <- "~/tmp_rprof"
+  my_filenames <- list.files(path_data, pattern="*.RDa", full.names = TRUE)
+  Rprof(rprof_tmp_output)
+  wiritttea::collect_files_n_taxa(filenames = my_filenames)
+  Rprof(NULL)
+  summaryRprof(rprof_tmp_output)
+}
