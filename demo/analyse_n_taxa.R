463a43feb043ffdd56241019a1b9f5887e1a59ab
richelbilderbeek <richel@richelbilderbeek.nl>
2017-05-11 09:36:00 +0200

    analyse_n_taxa on Peregrine

--- demo/analyse_n_taxa.R
+++ demo/analyse_n_taxa.R
@@ -4,6 +4,15 @@ options(warn = 2) # Be strict
 path_data <- "~/Peregrine20170509"
 n_taxa_filename <- "~/n_taxa.csv"
 
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) > 0) path_data <- args[1]
+if (length(args) > 1) n_taxa_filename <- args[2]
+
+print(paste("path_data:", path_data))
+print(paste("n_taxa_filename:", n_taxa_filename))
+
+
+
 # Create file if absent
 if (!file.exists(n_taxa_filename)) {
   my_filenames <- list.files(path_data, pattern = "*.RDa", full.names = TRUE)
