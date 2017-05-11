9b18ae8fed4985775d42bb24a4f1332e4a6b8c61
richelbilderbeek <richel@richelbilderbeek.nl>
2017-05-11 15:23:38 +0200

    analyse_alignments [skip ci]

---
+++ demo/analyse_alignments.R
@@ -0,0 +1,30 @@
+# Analyse if the alignments are created successfully
+library(wiritttea)
+options(warn = 2) # Be strict
+path_data <- "~/Peregrine20170509"
+
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) > 0) path_data <- args[1]
+
+print(paste("path_data:", path_data))
+
+my_filenames <- list.files(path_data, pattern = "*.RDa", full.names = TRUE)
+
+# my_filenames <- c(
+#   "/home/p230198/Peregrine20170509/article_1_3_0_3_0_958.RDa",
+#   "/home/p230198/Peregrine20170509/article_1_3_0_3_1_972.RDa",
+#   "/home/p230198/Peregrine20170509/article_1_3_0_3_1_976.RDa"
+# )
+
+
+for (my_filename in my_filenames) {
+  has_alignments <- FALSE
+  tryCatch( {
+      testit::assert(file.exists(my_filename))
+      file <- wiritttes::read_file(my_filename) # Can fail
+      has_alignments <- all(wiritttes::has_alignments(file))
+    },
+    error = function(cond) {} # OK
+  )
+  print(paste(my_filename, ": ", has_alignments, sep = ""))
+}
