badb63590c1c515eecfcff66fdf5ff56bc77df64
richelbilderbeek <richel@richelbilderbeek.nl>
2017-05-18 16:27:31 +0200

    Fixed 'is_good_alignment'

--- R/is_good_alignment.R
+++ R/is_good_alignment.R
@@ -7,5 +7,18 @@
 is_good_alignment <- function(
   alignment
 ) {
+  if (!ribir::is_alignment(alignment)) {
+    stop("Argument 'alignmnet' is not an alignment")
+  }
+  m <- ape::dist.dna(x = alignment, model = "JC69", as.matrix = TRUE)
+
+  # Detect if there is at least one NA
+  if (!all(!is.na(m))) {
+    return(FALSE)
+  }
+  # Detect zeroes, except those on the diagonal
+  if (sum(m == 0.0) - nrow(m) > 0) {
+    return(FALSE)
+  }
   return(TRUE)
 }
--- tests/testthat/test-is_good_alignment.R
+++ tests/testthat/test-is_good_alignment.R
@@ -23,8 +23,9 @@ test_that("is_good_alignment: good alignment", {
   wiritttes::add_alignments(filename = filename)
 
   # Alignment is OK
-  testthat::expect_true(
-    wiritttea::is_good_alignment(wiritttes::read_file(filename = filename)))
+  alignment <- wiritttes::get_alignment(
+    wiritttes::read_file(filename = filename), sti = 1, ai = 1)
+  testthat::expect_true(wiritttea::is_good_alignment(alignment))
 
   # Cleaning up
   file.remove(filename)
@@ -43,20 +44,22 @@ test_that("is_good_alignment: bad alignment, due to low mutation rate", {
     erg = 0.5,
     eri = 0.5,
     age = 5,
-    mutation_rate = 0.0,
+    mutation_rate = 1E-99, #Cannot be zero
     n_alignments = 1,
     sequence_length = 10,
     nspp = 10,
     n_beast_runs = 1,
     filename = filename
   )
+  testit::assert(wiritttes::is_valid_file(filename))
   wiritttes::add_pbd_output(filename)
   wiritttes::add_species_trees(filename = filename)
   wiritttes::add_alignments(filename = filename)
 
   # Alignment is bad
-  testthat::expect_false(
-    wiritttea::is_good_alignment(wiritttes::read_file(filename = filename)))
+  alignment <- wiritttes::get_alignment(
+    wiritttes::read_file(filename = filename), sti = 1, ai = 1)
+  testthat::expect_false(wiritttea::is_good_alignment(alignment))
 
   # Cleaning up
   file.remove(filename)
@@ -87,8 +90,9 @@ test_that("is_good_alignment: bad alignment, due to high mutation rate", {
   wiritttes::add_alignments(filename = filename)
 
   # Alignment is bad
-  testthat::expect_false(
-    wiritttea::is_good_alignment(wiritttes::read_file(filename = filename)))
+  alignment <- wiritttes::get_alignment(
+    wiritttes::read_file(filename = filename), sti = 1, ai = 1)
+  testthat::expect_false(wiritttea::is_good_alignment(alignment))
 
   # Cleaning up
   file.remove(filename)
