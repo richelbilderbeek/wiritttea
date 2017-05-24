bafce2b5d3708bc1928b9362727a58176919042c
richelbilderbeek <richel@richelbilderbeek.nl>
2017-05-24 13:07:50 +0200

    Corrected test

--- tests/testthat/test-read_collected_strees_identical.R
+++ tests/testthat/test-read_collected_strees_identical.R
@@ -4,10 +4,10 @@ test_that("use", {
 
   csv_filename <- wiritttea::find_path("collect_files_strees_identical.csv")
   strees_identical <- wiritttea::read_collected_strees_identical(csv_filename)
-  testthat::expect_equal(names(df), c(
+  testthat::expect_equal(names(strees_identical), c(
     "filename", "strees_identical"
     )
   )
-  testthat::expect_true(is.factor(df$filename))
-  testthat::expect_true(is.factor(df$strees_identical))
+  testthat::expect_true(is.factor(strees_identical$filename))
+  testthat::expect_true(is.factor(strees_identical$strees_identical))
 })
