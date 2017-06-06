ce8f72ca6f51080535623150254c124764ceabe3
richelbilderbeek <richel@richelbilderbeek.nl>
2017-06-06 16:16:04 +0200

    Analyse alignments' DMID for real

--- demo/analyse_alignments_dmid.R
+++ demo/analyse_alignments_dmid.R
@@ -14,7 +14,7 @@ print(paste("alignments_dmid_filename:", alignments_dmid_filename))
 
 if (!file.exists(alignments_dmid_filename)) {
 
-  my_filenames <- head(list.files(path_data, pattern = "*.RDa", full.names = TRUE))
+  my_filenames <- list.files(path_data, pattern = "*.RDa", full.names = TRUE)
 
   # Assume all files have the same number of alignments
   n_alignments <- wiritttes::extract_napst(wiritttes::read_file(my_filenames[1])) * 2
@@ -29,9 +29,7 @@ if (!file.exists(alignments_dmid_filename)) {
 
   for (row in seq(1, n_rows)) {
     file_index <- 1 + trunc((row - 1)/ 4)
-    print(file_index)
     my_filename <- my_filenames[file_index]
-    print(my_filename)
     testit::assert(file.exists(my_filename))
 
     tryCatch({
@@ -39,14 +37,9 @@ if (!file.exists(alignments_dmid_filename)) {
       this_n_alignments <- wiritttes::extract_napst(file) * 2
       testit::assert(this_n_alignments == n_alignments)
       ai <- 1 + ((row - 1 ) %% n_alignments)
-      print(paste("ai:", ai))
       alignment <- wiritttes::get_alignment_by_index(file, ai)
-      print("alignment")
       m <- ape::dist.dna(x = alignment, model = "JC69", as.matrix = TRUE)
-      print("m")
       dmid <- wiritttea::calc_dmid(m)
-      print("dmid: ")
-      print(dmid)
       df$dmid[row] <- dmid
     }, error = function(cond) {} #nolint
     )
