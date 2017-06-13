eb4a470f5c932f63812b57b9168320eb4567a073
richelbilderbeek <richel@richelbilderbeek.nl>
2017-06-13 12:05:37 +0200

    Use all files

--- demo/analyse_esses.R
+++ demo/analyse_esses.R
@@ -7,8 +7,6 @@ args <- commandArgs(trailingOnly = TRUE)
 if (length(args) > 0) path_data <- args[1]
 if (length(args) > 1) esses_filename <- args[2]
 
-
-
 # nstpist: Number of Species Trees Per Incipient Species Tree
 # napst: Number of Alignments per Species Tree
 # nppa: Number of Posteriors per Alignment
@@ -49,7 +47,7 @@ if (!file.exists(esses_filename)) {
   my_filenames <- list.files(
     path_data,
     pattern = "article_.*\\.RDa",
-    full.names = TRUE)[1]
+    full.names = TRUE)
 
   # nstpist: Number of Species Trees Per Incipient Species Tree
   nstpist <- 2
@@ -67,7 +65,7 @@ if (!file.exists(esses_filename)) {
     filename = rep(basename(my_filenames), each = nppf),
     sti = rep(seq(1,2), each = napst * nppa, times = n_files),
     ai = rep(seq(1, napst), each = nstpist, times = n_files * nppa),
-    pi = rep(seq(1, nppa), times = n_files * nstpist * napst * nppa),
+    pi = rep(seq(1, nppa), times = n_files * nstpist * napst),
     min_ess = rep(NA, n_rows)
   )
 
@@ -93,49 +91,6 @@ if (!file.exists(esses_filename)) {
       )
       index <- index + 1
     }
-  }
-  df
-  write.csv(df, esses_filename)
-}
-
-
-# Classic approach, takes 72 minutes per file
-if (!file.exists(esses_filename) && 1 == 2) {
-
-  my_filenames <- list.files(path_data, pattern = "*.RDa", full.names = TRUE)
-
-  # Assume all files have the same number of alignments
-
-  # nstpist: Number of Species Trees Per Incipient Species Tree
-  nstpist <- 2
-  # napst: Number of Alignments per Species Tree
-  napst <- wiritttes::extract_napst(wiritttes::read_file(my_filenames[1]))
-  # nppa: Number of Posteriors per Alignment
-  nppa <- wiritttes::extract_nppa(wiritttes::read_file(my_filenames[1]))
-  # nppf: Number of Posteriors Per File
-  nppf <- nstpist * napst * nppa
-  n_files <- length(my_filenames)
-  n_rows <- n_files * nppf
-
-  df <- data.frame(
-    filename = rep(basename(my_filenames), each = nppf),
-    sti = rep(seq(1,2), each = napst * nppa, times = n_files),
-    ai = rep(seq(1, napst), each = nstpist, times = n_files * nppa),
-    pi = rep(seq(1, nppa), times = n_files * nstpist * napst * nppa),
-    ess = rep(NA, n_rows)
-  )
-
-  for (i in seq_along(my_filenames))
-  {
-    my_filename <- my_filenames[i]
-    index_from <- ((i - 1) * nppf) + 1
-    index_to <- (i * nppf)
-    print(paste(my_filename, index_from, index_to))
-    tryCatch({
-      df_sub <- wiritttea::collect_file_esses(my_filename)
-      df[, index_from:index_to] <- df_sub
-    }, error = function(cond) {} # nolint
-    )
   }
 
   write.csv(df, esses_filename)
