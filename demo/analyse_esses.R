8d001ec3418a26e5e65cebf501cde739da2ee691
richelbilderbeek <richel@richelbilderbeek.nl>
2017-06-14 13:45:00 +0200

    Increase tieme limit

--- demo/analyse_esses.R
+++ demo/analyse_esses.R
@@ -2,10 +2,14 @@
 options(warn = 2) # Be strict
 path_data <- "~/Peregrine20170710"
 esses_filename <- "~/esses_20170710.csv"
+use_classic <- FALSE
+
 
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) > 0) path_data <- args[1]
 if (length(args) > 1) esses_filename <- args[2]
+if (length(args) > 2) use_classic <- ifelse(args[3] == "TRUE", TRUE, FALSE)
+
 
 # nstpist: Number of Species Trees Per Incipient Species Tree
 # napst: Number of Alignments per Species Tree
@@ -42,7 +46,9 @@ test_create_log_filenames()
 # Create file if absent
 
 # New approach, reads the files created by BEAST
-if (!file.exists(esses_filename)) {
+if (!file.exists(esses_filename) && !use_classic) {
+
+  print("Use new approach")
 
   my_filenames <- list.files(
     path_data,
@@ -68,7 +74,6 @@ if (!file.exists(esses_filename)) {
     pi = rep(seq(1, nppa), times = n_files * nstpist * napst),
     min_ess = rep(NA, n_rows)
   )
-
 
   index <- 1
   for (i in seq_along(my_filenames)) {
@@ -96,32 +101,68 @@ if (!file.exists(esses_filename)) {
   write.csv(df, esses_filename)
 }
 
+# Classic approach, takes 72 minutes per file
+if (!file.exists(esses_filename) && use_classic) {
+
+  print("Use classic approach")
+
+  my_filenames <- list.files(path_data, pattern = "*.RDa", full.names = TRUE)
+
+  # Assume all files have the same number of alignments
+
+  # nstpist: Number of Species Trees Per Incipient Species Tree
+  nstpist <- 2
+  # napst: Number of Alignments per Species Tree
+  napst <- wiritttes::extract_napst(wiritttes::read_file(my_filenames[1]))
+  # nppa: Number of Posteriors per Alignment
+  nppa <- wiritttes::extract_nppa(wiritttes::read_file(my_filenames[1]))
+  # nppf: Number of Posteriors Per File
+  nppf <- nstpist * napst * nppa
+  n_files <- length(my_filenames)
+  n_rows <- n_files * nppf
+
+  df <- data.frame(
+    filename = rep(basename(my_filenames), each = nppf),
+    sti = rep(seq(1,2), each = napst * nppa, times = n_files),
+    ai = rep(seq(1, napst), each = nstpist, times = n_files * nppa),
+    pi = rep(seq(1, nppa), times = n_files * nstpist * napst),
+    min_ess = rep(NA, n_rows)
+  )
+  testit::assert(names(df) == c("filename", "sti", "ai", "pi", "min_ess"))
+
+  for (i in seq_along(my_filenames))
+  {
+    my_filename <- my_filenames[i]
+    index_from <- ((i - 1) * nppf) + 1
+    index_to <- (i * nppf)
+    tryCatch({
+      df_sub <- wiritttea::collect_file_esses(my_filename)
+      df[index_from:index_to, ] <- df_sub
+    }, error = function(cond) {} # nolint
+    )
+  }
+
+  testit::assert(names(df) == c("filename", "sti", "ai", "pi", "min_ess"))
+  write.csv(df, esses_filename)
+}
+
+
 #Investigations
 df_esses <- wiritttea::read_collected_esses(esses_filename)
 
-# Plotting ESSes as density plot
-if (1 == 2) {
-  ggplot2::ggplot(df_esses,  ggplot2::aes(min_ess)) +
-    ggplot2::geom_density(alpha = 0.25, ggplot2::aes(y = ..density..), position = 'identity')
-}
+# How many NA's?
+library(dplyr)
+knitr::kable(df_esses  %>% count(is.na(min_ess)))
 
+# Plotting ESSes distributions
+#   as density plot
+ggplot2::ggplot(subset(df_esses, !is.na(min_ess)),
+  ggplot2::aes(min_ess)) +
+  ggplot2::geom_density(alpha = 0.25, ggplot2::aes(y = ..density..), position = 'identity') +
+  ggplot2::labs(title = "Effective Sample Sizes", x = "Effective Sample Size", y = "Density")
+#   as histogram
+ggplot2::ggplot(subset(df_esses, !is.na(min_ess)),
+  ggplot2::aes(min_ess)) +
+  ggplot2::geom_histogram() +
+  ggplot2::labs(title = "Effective Sample Sizes", x = "Effective Sample Size", y = "Count")
 
-if (1 == 2) {
-  df_n_taxa <- read.csv("~/n_taxa.csv")
-
-  names(df_n_taxa)
-  df_n_taxa$filename <- as.factor(df_n_taxa$filename)
-
-  head(df_n_taxa)
-  df_esses
-}
-
-# Profiling
-if (1==2) {
-  rprof_tmp_output <- "~/tmp_rprof"
-  Rprof(rprof_tmp_output)
-  my_filenames <- list.files(path_data, pattern="*.RDa", full.names=TRUE)
-  wiritttea::collect_files_esses(filenames = my_filenames)
-  Rprof(NULL)
-  summaryRprof(rprof_tmp_output)
-}
