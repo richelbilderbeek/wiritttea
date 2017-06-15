91d2e7d902be962d5fa7da74aa5165a078b9c1e1
richelbilderbeek <richel@richelbilderbeek.nl>
2017-06-15 12:13:10 +0200

    Simplified script

--- demo/analyse_alignments.R
+++ demo/analyse_alignments.R
@@ -14,45 +14,7 @@ print(paste("alignments_filename:", alignments_filename))
 
 if (!file.exists(alignments_filename)) {
   my_filenames <- list.files(path_data, pattern = "*.RDa", full.names = TRUE)
-
-  df <- data.frame(
-    filename = basename(my_filenames),
-    n_alignments_ok = rep(0, length(my_filenames)),
-    n_alignments_zeroes = rep(0, length(my_filenames)),
-    n_alignments_na = rep(0, length(my_filenames))
-  )
-
-  for (i in seq_along(my_filenames)) {
-
-    my_filename <- my_filenames[i]
-    #print(my_filename)
-    testit::assert(file.exists(my_filename))
-
-    tryCatch( {
-        file <- wiritttes::read_file(my_filename) # Can fail
-        n_alignments <- wiritttes::extract_napst(file) * 2
-        for (j in seq(1, n_alignments)) {
-          #print(j)
-          alignment <- wiritttes::get_alignment_by_index(file, j)
-          m <- ape::dist.dna(x = alignment, model = "JC69", as.matrix = TRUE)
-          #print(m)
-          # Detect if there is at least one NA
-          if (!all(!is.na(m))) {
-            df$n_alignments_na[i] <- df$n_alignments_na[i] + 1
-          } else if (sum(m == 0.0) - nrow(m) > 0) {
-            # Detect zeroes, except those on the diagonal
-            df$n_alignments_zeroes[i] <- df$n_alignments_zeroes[i] + 1
-          } else {
-            df$n_alignments_ok[i] <- df$n_alignments_ok[i] + 1
-          }
-          gc() # Need to do so manually
-        }
-      },
-      error = function(cond) {} #nolint
-    )
-  }
-
-  # Save
+  df <- collect_files_alignments(my_filenames)
   write.csv(df, alignments_filename)
 }
 
