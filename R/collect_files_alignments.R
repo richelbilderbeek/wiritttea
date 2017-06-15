77654f0ffac4ee52b2e135754828b76c6c66272c
richelbilderbeek <richel@richelbilderbeek.nl>
2017-06-15 12:17:52 +0200

    Improved doc

--- R/collect_files_alignments.R
+++ R/collect_files_alignments.R
@@ -1,11 +1,22 @@
 #' Collects the quality of the alignments of the files
 #' @param filenames names of the parameter files
-#' @return A dataframe of the quality of the alignments
+#' @return A dataframe with the columns 'filename', 'n_alignments_ok' (number
+#'   of alignments that have a distance matrix with only non-zero and non-NA
+#'   elements, except on the diagonal), 'n_alignments_zero' (number
+#'   of alignments that have a distance matrix with zeroes next to the
+#'   diagonal, indicating two identical DNA sequences in the alignment),
+#'   and 'n_alignments_na' (number
+#'   of alignments that have a distance matrix with NAs,
+#'   indicating that two DNA sequences are two dissimilar to have
+#'   their Jukes-Cantor distance measured)
 #' @examples
 #'   filenames <- find_paths(c("toy_example_3.RDa", "toy_example_4.RDa"))
 #'   df <- collect_files_alignments(filenames)
 #'   testit::assert(nrow(df) == 2)
-#'   expected_names <- c("filename", "n_alignments_ok", "n_alignments_zeroes",
+#'   expected_names <- c(
+#'     "filename",
+#'     "n_alignments_ok",
+#'     "n_alignments_zeroes",
 #'     "n_alignments_na")
 #'   testit::assert(all.equal(names(df), expected_names))
 #' @export
