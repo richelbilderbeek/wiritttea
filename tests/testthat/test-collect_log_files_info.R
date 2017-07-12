dedbd2938750d52841b7f580b09059daeed4db4a
richelbilderbeek <richel@richelbilderbeek.nl>
2017-07-12 13:29:57 +0200

    Test collect_log_files_info

--- tests/testthat/test-collect_log_files_info.R
+++ tests/testthat/test-collect_log_files_info.R
@@ -1,5 +1,13 @@
 context("collect_log_files_info")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("example", {
+
+    filenames <- c(
+     find_path("add_alignments_ok.log"),
+     find_path("add_alignments_exceeded_memory.log")
+   )
+   df <- collect_log_files_info(filenames)
+   testthat::expect_equivalent(names(df), c("filename", "exit_status"))
+   testthat::expect_equal(nrow(df), length(filenames))
+   testthat::expect_true(df$exit_status[1] != df$exit_status[2])
 })
