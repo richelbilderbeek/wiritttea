7217209c1f0237d14f7cbd374abf874bc4a0e948
richelbilderbeek <richel@richelbilderbeek.nl>
2017-07-12 16:37:12 +0200

    EOD

--- demo/analyse_log_files.R
+++ demo/analyse_log_files.R
@@ -28,3 +28,13 @@ if (!file.exists(log_files_filename)) {
 print("Load log files info")
 df_log_files <- wiritttea::read_collected_log_files_info(log_files_filename)
 
+# Why did these fail?
+head(df_log_files)
+ggplot2::ggplot(
+  data = df_log_files,
+  ggplot2::aes(x = exit_status)
+) +
+  ggplot2::geom_bar() +
+  ggplot2::scale_y_log10() +
+  ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1))
+
