6a0813554e2eeef027ccda7d28ddcce56d1425eb
richelbilderbeek <richel@richelbilderbeek.nl>
2017-07-19 10:23:49 +0200

    collect_files_operators works on empty files as well

--- R/collect_files_operators.R
+++ R/collect_files_operators.R
@@ -1,5 +1,6 @@
 #' Calculates the BEAST2 operators from multiple files
 #' @param filenames the names of the .xml.state files
+#' @param show_progress shows the progress if TRUE
 #' @return a data frame with the operators' acceptances
 #' @export
 #' @examples
@@ -8,13 +9,21 @@
 #'   )
 #' @author Richel Bilderbeek
 collect_files_operators <- function(
-  filenames = rep(find_path("collect_files_operators.xml.state"), 2)) {
+  filenames = find_paths(c("collect_files_operators.xml.state", "collect_files_operators_empty.xml.state")),
+  show_progress = FALSE) {
 
   # Calculate the number of rows needed
   n_rows <- 0
   for (filename in filenames) {
-    n_rows <- n_rows + nrow(RBeast::parse_beast_state_operators(filename))
-
+    if (show_progress == TRUE) {
+      print(filename)
+    }
+    n_rows_to_add <- 1
+    tryCatch( {
+      n_rows_to_add <- nrow(RBeast::parse_beast_state_operators(filename))
+      }, error = function(cond) {} # nolint
+    )
+    n_rows <- n_rows + n_rows_to_add
   }
   df <- data.frame(
     operator = rep(NA, n_rows),
@@ -31,12 +40,23 @@ collect_files_operators <- function(
 
   index <- 1
   for (filename in filenames) {
-    this_df <- RBeast::parse_beast_state_operators(filename)
-    this_df$filename = basename(filename)
-    df[
-      index:(index + nrow(this_df) - 1),
-    ] <- this_df
-    index <- index + nrow(this_df)
+    if (show_progress == TRUE) {
+      print(filename)
+    }
+    indices_to_add <- 1
+    tryCatch( {
+      this_df <- RBeast::parse_beast_state_operators(filename)
+      this_df$filename <- basename(filename)
+      df[
+        index:(index + nrow(this_df) - 1),
+      ] <- this_df
+      indices_to_add <- nrow(this_df)
+    }, error = function(cond) {} # nolint
+    )
+    if (indices_to_add == 1) {
+      df$filename[index] <- basename(filename)
+    }
+    index <- index + indices_to_add
   }
 
   # Put filename in the front
--- tests/testthat/test-collect_files_operators.R
+++ tests/testthat/test-collect_files_operators.R
@@ -2,12 +2,24 @@ context("collect_files_operators")
 
 test_that("example", {
 
-  df_operators <- collect_files_operators(
-    filenames = rep(find_path("collect_files_operators.xml.state"), 2)
+  df_operators <- wiritttea::collect_files_operators(
+    filenames = rep(wiritttea::find_path("collect_files_operators.xml.state"), 2)
   )
   expected_names <- c("filename", "operator", "p", "accept", "reject", "acceptFC",
     "rejectFC", "rejectIv", "rejectOp")
   testthat::expect_equal(names(df_operators), expected_names)
 
   testthat::expect_equal(nrow(df_operators), 18)
+})
+
+test_that("good and empty file", {
+
+  df_operators <- wiritttea::collect_files_operators(
+    filenames =
+      wiritttea::find_paths(
+        c("collect_files_operators.xml.state", "collect_files_operators_empty.xml.state"))
+
+  )
+
+  testthat::expect_equal(nrow(df_operators), 10)
 })
