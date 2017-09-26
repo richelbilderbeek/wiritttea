e5540bcf3480e06cc8bfb1ebc69eda7dc023e18e
richelbilderbeek <richel@richelbilderbeek.nl>
2017-09-26 14:48:38 +0200

    analyse_nltt_stats

--- scripts/collect_nltt_stats.R
+++ scripts/collect_nltt_stats.R
@@ -1,23 +1,29 @@
+# Analyse the number of taxa
 library(wiritttea)
-folder <- "/data/p230198"
+options(warn = 2) # Be strict
 
-# filenames
-fns <- paste(
-  folder, list.files(folder, pattern = "\\.RDa"), sep = "/"
-)
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+  stop("Supply a source folder as a first argument, e.g. '~/wirittte_data/stub'")
+}
+if (length(args) == 1) {
+  stop("Supply a target filename as a second argument, e.g. '~/nltt_stats_stub.csv'")
+}
+if (length(args) != 2) {
+  stop("Supply two parameters: a source folder and a target filename, ",
+    "e.g. '~/wirittte_data/stub ~/parameters_stub.csv'")
+}
 
-df <- collect_files_nltt_stats(fns)
+path_data <- args[1]
+nltt_stats_filename <- args[2]
 
-utils::write.csv(
-  x = df,
-  file = "collect_files_nltt_stats.csv",
-  row.names = TRUE
-)
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) > 0) path_data <- args[1]
+if (length(args) > 1) nltt_stats_filename <- args[2]
 
-utils::write.csv(
-  x = df,
-  file = "../inst/extdata/collect_files_nltt_stats.csv",
-  row.names = TRUE
-)
+print(paste("path_data:", path_data))
+print(paste("nltt_stats_filename:", nltt_stats_filename))
 
-warnings()
+my_filenames <- list.files(path_data, pattern = "*.RDa", full.names = TRUE)
+df_nltt_stats <- wiritttea::collect_files_nltt_stats_dirty(my_filenames)
+write.csv(df_nltt_stats, nltt_stats_filename)
