510a49b1528e15a597f84e0fce8557ba0678abd5
richelbilderbeek <richel@richelbilderbeek.nl>
2017-09-26 13:18:59 +0200

    collect_n_species_trees works:

--- scripts/collect_n_species_trees.R
+++ scripts/collect_n_species_trees.R
@@ -1,20 +1,38 @@
+# Analyse the number of taxa
 library(wiritttea)
-folder <- "/data/p230198"
-fns <- paste(
-  folder, list.files(folder, pattern = "\\.RDa"), sep = "/"
-)
-df <- wiritttea::collect_files_n_species_trees(fns)
+options(warn = 2) # Be strict
 
-utils::write.csv(
-  x = df,
-  file = "collect_files_n_species_trees.csv",
-  row.names = TRUE
-)
+# Collects the number of species trees used in all .RDa files in one file
+library(wiritttea)
+options(warn = 2)
 
-utils::write.csv(
-  x = df,
-  file = "../inst/extdata/collect_files_n_species_trees.csv",
-  row.names = TRUE
-)
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+  stop("Supply a source folder as a first argument, e.g. '~/wirittte_data/stub'")
+}
+if (length(args) == 1) {
+  stop("Supply a target filename as a second argument, e.g. '~/n_species_trees_stub.csv'")
+}
+if (length(args) != 2) {
+  stop("Supply two parameters: a source folder and a target filename, ",
+    "e.g. '~/wirittte_data/stub ~/parameters_stub.csv'")
+}
 
-warnings()
+path_data <- args[1]
+n_species_trees_filename <- args[2]
+
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) > 0) path_data <- args[1]
+if (length(args) > 1) n_species_trees_filename <- args[2]
+
+print(paste("path_data:", path_data))
+print(paste("n_species_trees_filename:", n_species_trees_filename))
+
+print("Collecting .RDa files")
+my_filenames <- list.files(path_data, pattern = "*.RDa", full.names = TRUE)
+
+print("Collecting # taxa")
+df_n_species_trees <- wiritttea::collect_files_n_species_trees(filenames = my_filenames)
+
+print("Saving # taxa")
+write.csv(df_n_species_trees, n_species_trees_filename)
