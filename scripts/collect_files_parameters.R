fb89a21872580ffad0a5e7ed7b7582ab78ed0459
richelbilderbeek <richel@richelbilderbeek.nl>
2017-09-26 10:51:42 +0200

    scripts/run collects the file parameters, removed vignette showing the collected parameters

--- scripts/collect_files_parameters.R
+++ scripts/collect_files_parameters.R
@@ -1,5 +1,13 @@
+# Collects the parameters used in all .RDa files in one file
 library(wiritttea)
-folder <- "/data/p230198"
+
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+  stop("Supply a source folder")
+}
+
+folder <- args[1]
+
 fns <- paste(
   folder, list.files(folder, pattern = "\\.RDa"), sep = "/"
 )
@@ -7,24 +15,6 @@ df <- wiritttea::collect_files_parameters(fns)
 
 utils::write.csv(
   x = df,
-  file = "collect_files_parameters.csv",
+  file = "~/collect_files_parameters.csv",
   row.names = TRUE
 )
-
-utils::write.csv(
-  x = df,
-  file = "../inst/extdata/collect_files_parameters.csv",
-  row.names = TRUE
-)
-
-tryCatch(
-  rmarkdown::render("../vignettes/analyse_files.Rmd", output_file = "~/analyse_files.html"),
-  error = function(msg) { message(msg) }
-)
-
-tryCatch(
-  system("pandoc ~/analyse_files.html -o analyse_files.pdf"),
-  error = function(msg) { message(msg) }
-)
-
-warnings()
