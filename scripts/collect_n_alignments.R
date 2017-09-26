0c9816c892d36e7554b9ed78c60b66f57bf9f9c1
richelbilderbeek <richel@richelbilderbeek.nl>
2017-09-26 13:28:47 +0200

    collect_n_alignments

--- scripts/collect_n_alignments.R
+++ scripts/collect_n_alignments.R
@@ -7,7 +7,7 @@ if (length(args) == 0) {
   stop("Supply a source folder as a first argument, e.g. '~/wirittte_data/stub'")
 }
 if (length(args) == 1) {
-  stop("Supply a target filename as a second argument, e.g. '~/n_species_trees_stub.csv'")
+  stop("Supply a target filename as a second argument, e.g. '~/n_alignments_stub.csv'")
 }
 if (length(args) != 2) {
   stop("Supply two parameters: a source folder and a target filename, ",
@@ -15,20 +15,20 @@ if (length(args) != 2) {
 }
 
 path_data <- args[1]
-n_species_trees_filename <- args[2]
+n_alignments_filename <- args[2]
 
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) > 0) path_data <- args[1]
-if (length(args) > 1) n_species_trees_filename <- args[2]
+if (length(args) > 1) n_alignments_filename <- args[2]
 
 print(paste("path_data:", path_data))
-print(paste("n_species_trees_filename:", n_species_trees_filename))
+print(paste("n_alignments_filename:", n_alignments_filename))
 
 print("Collecting .RDa files")
 my_filenames <- list.files(path_data, pattern = "*.RDa", full.names = TRUE)
 
-print("Collecting # taxa")
-df_n_species_trees <- wiritttea::collect_files_n_species_trees(filenames = my_filenames)
+print("Collecting # alignments")
+df_n_alignments <- wiritttea::collect_files_n_alignments(filenames = my_filenames)
 
-print("Saving # taxa")
-write.csv(df_n_species_trees, n_species_trees_filename)
+print("Saving # alignments")
+write.csv(df_n_alignments, n_alignments_filename)
