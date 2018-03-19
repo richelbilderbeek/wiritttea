1b4538857a245b62cbcfe902f0d65fd6e3ef8e4c
richelbilderbeek <richel@richelbilderbeek.nl>
2018-03-19 20:54:49 +0100

    Correct namespace

--- tests/testthat/test-create_figure_error_bd_mean.R
+++ tests/testthat/test-create_figure_error_bd_mean.R
@@ -6,7 +6,7 @@ test_that("works", {
   testthat::expect_false(file.exists(filename))
 
   testthat::expect_silent(
-    wiritttea::create_figure_error_bd_mean(
+    wiritttea:::create_figure_error_bd_mean(
       parameters = wiritttea::read_collected_parameters(),
       nltt_stats = wiritttea::read_collected_nltt_stats(burn_in_fraction = 0.2),
       filename = filename
